fdc635516faf770796fbbad0798449c43a79cf0c
dattt0711 <1801040060@s.hanu.edu.vn>
2024-12-15 20:31:08 +0700

    Init project 3 udacity

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=test123456
+export POSTGRES_HOST=db-udacity-study-v1.csp1jw63x9ve.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=dat-udacity
+export AWS_BUCKET_ACCESS_POINT=arn:aws:s3:::dat-udacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
